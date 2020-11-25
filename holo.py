822fe63d82066f37e50cf5249a3141ba00602f51
Parviz Azimov <parviz.azimov@student.lamk.fi>
2020-11-25 10:42:59 +0200

    changed the format of the output to be numbered

--- holo.py
+++ holo.py
@@ -11,12 +11,13 @@ response = requests.get(url, headers={'User-Agent':ua.chrome})
 soup = BeautifulSoup(response.text, 'html.parser')
 streams = soup.find_all("a", class_="thumbnail", style=lambda value: value and 'border: 3px red solid' in value)
 print("currently live: "+str(len(streams)))
+i = 0
 for stream in streams:
     idol = " ".join(stream.text.split())
-    print(idol + " / romaji: "+romkan.to_roma(idol)[6:])
+    i += 1
+    print(str(i)+'. '+idol + " / romaji: "+romkan.to_roma(idol)[6:])
 name = input("input the name of the holo: ")
 cmd=''
-i = 0
 for stream in streams:
     if stream.find(text=re.compile(name)):
         vid = stream["href"]
