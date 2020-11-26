85d611714cf2e641299c6b907c072ae00e43d677
Azimov Parviz <parviz.azimov@student.lamk.fi>
2020-11-26 20:50:32 +0200

    outputs the currently live streams after you close the mpv window

--- holo.py
+++ holo.py
@@ -5,23 +5,26 @@ import requests
 import re
 import romkan
 
+def list_streams(streams):
+    print("currently live: "+str(len(streams)))
+    i = 0
+    for stream in streams:
+        idol = " ".join(stream.text.split())
+        i += 1
+        print(str(i)+'. '+idol + " / romaji: "+romkan.to_roma(idol)[6:])
+
 ua = UserAgent(verify_ssl=False)
 url="https://schedule.hololive.tv/lives/all"
 response = requests.get(url, headers={'User-Agent':ua.chrome})
 soup = BeautifulSoup(response.text, 'html.parser')
 streams = soup.find_all("a", class_="thumbnail", style=lambda value: value and 'border: 3px red solid' in value)
-print("currently live: "+str(len(streams)))
-i = 0
-for stream in streams:
-    idol = " ".join(stream.text.split())
-    i += 1
-    print(str(i)+'. '+idol + " / romaji: "+romkan.to_roma(idol)[6:])
 cmd=''
 while(True):
+    list_streams(streams)
     name = input("input the name of the holo: ")
     for stream in streams:
         if stream.find(text=re.compile(name)):
             vid = stream["href"]
             cmd = 'mpv '+vid
             print("cmd is "+cmd)
-            subprocess.Popen(cmd, shell=True)
+            subprocess.call(cmd, shell=True)
