f07e868104205b1e3a6a3b7605c15dc4e5cdca23
Azimov Parviz <parviz.azimov@student.lamk.fi>
2020-12-29 13:25:20 +0200

    Added a dictionary of the members so you don't need to use the japanese names to access the streams. Just type their official english name (e.g Polka, Rushia, Fubuki).

--- holo.py
+++ holo.py
@@ -5,6 +5,42 @@ import requests
 import re
 import romkan
 
+
+member_list = {
+    "Sora" : "ときのそら",
+    "Roboco" : "ロボ子",
+    "Miko" : "さくらみこ",
+    "Suisei" : "星街すいせい",
+    "Mel" : "夜空メル",
+    "Fubuki" : "白上フブキ",
+    "Matsuri" : "夏色まつり",
+    "Aki" : "アキ・ローゼンタール",
+    "Haachama" : "赤井はあと",
+    "Aqua" : "湊あくあ",
+    "Shion" : "紫咲シオン",
+    "Ayame" : "百鬼あやめ",
+    "Choco" : "癒月ちょこ",
+    "Subaru" : "大空スバル",
+    "Mio" : "大神ミオ",
+    "Okayu" : "猫又おかゆ",
+    "Korone" : "戌神ころね",
+    "Pekora" : "兎田ぺこら",
+    "Rushia" : "潤羽るしあ",
+    "Flare" : "不知火フレア",
+    "Marine" : "宝鐘マリン",
+    "Noel" : "白銀ノエル",
+    "Kanata" : "天音かなた",
+    "Coco" : "桐生ココ",
+    "Watame" : "角巻わため",
+    "Towa" : "常闇トワ",
+    "Luna" : "姫森ルーナ",
+    "Lamy" : "雪花ラミィ",
+    "Nene" : "桃鈴ねね",
+    "Botan" : "獅白ぼたん",
+    "Aloe" : "魔乃アロエ",
+    "Polka" : "尾丸ポルカ"
+    }
+
 def list_streams(streams):
     print("currently live: "+str(len(streams)))
     i = 0
@@ -22,6 +58,8 @@ while(True):
     streams = soup.find_all("a", class_="thumbnail", style=lambda value: value and 'border: 3px red solid' in value)
     list_streams(streams)
     name = input("input the name of the holo: ")
+    if name in member_list:
+        name = member_list[name]
     for stream in streams:
         if stream.find(text=re.compile(name)):
             vid = stream["href"]
