dd8e1a7f2bd986b57508bb41a8bd12daaefb1b41
Azimov Parviz <parviz.azimov@student.lamk.fi>
2021-05-02 11:31:07 +0300

    changed the method of finding the streams. Now the program scrapes only the streams that happen today, whether they are live now or will come live on this day

--- holo.py
+++ holo.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-#!/usr/bin/env python
-=======
 #!/usr/bin/env python3
->>>>>>> a63b5b93176740338467a9fac114581113bd142b
 import subprocess
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
@@ -58,26 +54,33 @@ member_list = {
     }
 
 def list_streams(streams, sv):
-    print("currently live: "+str(len(streams)))
     i = 0
+    now = datetime.datetime.now()
+    now = now.replace(hour=now.time().hour, minute=now.time().minute, second=now.time().second, microsecond=0)#changing the format of the current time to match the time format of the schedule website
+    # print("idol time is: "+idol_time)
+    # print("current time is: "+str(now))
+    today = datetime.date.today()
+    if sv == 'border: 3px red solid':
+        print("currently live: "+str(len(streams)))
+    elif sv == 'border: 0':
+        print("upcoming streams: ")
     for stream in streams:
-        idol = " ".join(stream.text.split())
-        idol_en = idol[6:]#removing the time of the broadcast from html elemenet containing the streamer's name
-        idol_time = idol[:5]+':00'#getting the time of the broadcast
-        now = datetime.datetime.now()
-        current_time = datetime.datetime.strptime(idol_time,"%H:%M:%S")
-        current_time = now.replace(hour=current_time.time().hour, minute=current_time.time().minute, second=current_time.time().second, microsecond=0)#changing the format of the current time to match the time format of the schedule website
+        #idol = " ".join(stream.text.split())
+        idol_en = stream[6:]#removing the time of the broadcast from html elemenet containing the streamer's name
+        idol_time = stream[:5]+':00'#getting the time of the broadcast                
         i += 1
-        if sv == 'border: 3px red solid':
+        if sv == 'border: 3px red solid':            
             for key, name in member_list.items():
-                if name in idol:
+                if name in stream:
                     idol_en = key
-            print(str(i)+'. '+idol + " / romaji: "+ idol_en)
+            print(str(i)+'. '+stream + " / romaji: "+ idol_en)
         elif sv == 'border: 0':
             for key, name in member_list.items():
-                if (name in idol) & (idol_time>=str(current_time)):
+                if (name in stream) & (idol_time>=str(now)):
+                    print("idol time is "+idol_time)
+                    print("the time now is "+str(now))
                     idol_en = key
-                    print(str(i)+'. '+idol + " / romaji: "+ idol_en)
+                    print(str(i)+'. '+stream + " / romaji: "+ idol_en)
 
 def main(argv):
     search_value = ""#variable that will be used to determine whether live or upcoming livestreams are to be shown
@@ -97,35 +100,62 @@ def main(argv):
     ua = UserAgent(verify_ssl=False)
     url="https://schedule.hololive.tv/lives/all"
     cookies = {'timezone':'Europe/Helsinki'}
+    today = datetime.date.today().day
+    month = datetime.date.today().month
+    is_today = None
+    streams = []
     while(True):#while(True) so the user can repeatedly fetch new information from the schedule website after closing the stream they were watching before
         cmd=''
         response = requests.get(url, headers={'User-Agent':ua.chrome}, cookies=cookies)#making a request to the schedule website, using chrome headers from fake-useragent and cookies for the timezone
         soup = BeautifulSoup(response.text, 'html.parser')
-        streams = soup.find_all("a", class_="thumbnail", style=lambda value: value and search_value in value)#finding the streams based on the option passed to the program
+        rows = soup.find_all("div", class_="container", style=None)
+        for row in rows:
+            if (row.find("div", class_="holodule navbar-text")):
+                date = row.find("div", class_="holodule navbar-text")
+                if str(today) in str(date):
+                    is_today = True
+                    print(date)
+                else:
+                    is_today = None            
+            if is_today:
+                col = row.find("div",class_="row")
+                st = col.find_all("a", class_="thumbnail")
+                if st:
+                    for c in st:
+                        today_stream = c
+                        if today_stream:
+                            idol = " ".join(today_stream.text.split())
+                            streams += idol
+                            print(idol)
+        # streams = soup.find_all("a", class_="thumbnail", style=lambda value: value and search_value in value)#finding the streams based on the option passed to the program
+        dates = soup.find_all("div", class_="holodule navbar-text")
         list_streams(streams, search_value)
-        name = input("input the name of the holo(s) separated by space: ")
-        if " " in name:#case of passing several vtubers to open multiple streams
-            name = name.split(" ")
-            for item in name:
-                print(item)
-            for stream in streams:
+        if "red solid" in search_value:
+            name = input("input the name of the holo(s) separated by space: ")            
+            if " " in name:#case of passing several vtubers to open multiple streams
+                name = name.split(" ")
                 for item in name:
-                    if item in member_list:
-                        item = member_list[item]
-                    if stream.find(text=re.compile(item)):
-                        vid = stream["href"]
-                        cmd += 'mpv --really-quiet '+vid+' & '
+                    print(item)
+                for stream in streams:
+                    for item in name:
+                        if item in member_list:
+                            item = member_list[item]
+                        if stream.find(text=re.compile(item)):
+                            vid = stream["href"]
+                            cmd += 'mpv --really-quiet '+vid+' & '
 
-        else:#when only a single name is passed
-            if name in member_list:
-                name = member_list[name]
-            for stream in streams:
-                if stream.find(text=re.compile(name)):
-                    vid = stream["href"]
-                    cmd += 'mpv --really-quiet '+vid
-        subprocess.call(cmd, shell=True)#executing the final command containing either one or several streams
-        os.system('stty sane')#sometimes the inputted name(s) do not show up in the terminal
-        #(may be related to system slowing down after having too many instances of mpv open
-        #requires more testing
+            else:#when only a single name is passed
+                if name in member_list:
+                    name = member_list[name]
+                for stream in streams:
+                    if stream.find(text=re.compile(name)):
+                        vid = stream["href"]
+                        cmd += 'mpv --really-quiet '+vid
+            subprocess.call(cmd, shell=True)#executing the final command containing either one or several streams
+            os.system('stty sane')#sometimes the inputted name(s) do not show up in the terminal
+            #(may be related to system slowing down after having too many instances of mpv open
+            #requires more testing
+        else:
+            break
 if __name__ == "__main__":
    main(sys.argv[1:])
